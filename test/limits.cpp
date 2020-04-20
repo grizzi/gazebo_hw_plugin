d88e97c3da8b73538e5e97b965fe2c03860edfd4
giuserizz <grizzi@leggedrobotics.com>
2020-04-20 12:19:17 +0200

    rostest joint hard limits

--- test/limits.cpp
+++ test/limits.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <urdf/model.h>
+#include <ros/ros.h>
 
 #include "gazebo_hw_plugin/joint_data.hpp"
 #include "gazebo_hw_plugin/joint_data_group.hpp"
@@ -15,40 +16,89 @@
 
 // check this http://docs.ros.org/melodic/api/joint_limits_interface/html/c++/index.html
 
-TEST(LimitsTest, hardLimitsUrdf){
+TEST(LimitsTest, hardLimitsJointUrdf){
   urdf::Model model;
-  std::string path = ros::package::getPath("gazebo_hw_plugin");
-  path += "/resources/model.urdf";
-  model.initFile(path);
+//  <limit effort="30" velocity="1.0" lower="-2.2" upper="0.7" />
+  ASSERT_TRUE(model.initParam("/robot_description"));
 
   gazebo_hw_plugin::JointData joint("joint1");
+  joint.initLimits(&model);
   ASSERT_TRUE(joint.hasLimits());
 
-  EXPECT_EQ(0.0, joint.getEffortLimit());
-  EXPECT_EQ(0.0, joint.getUpperLimit());
-  EXPECT_EQ(0.0, joint.getLowerLimit());
+  EXPECT_EQ(30.0, joint.getEffortLimit());
+  EXPECT_EQ(0.7, joint.getUpperLimit());
+  EXPECT_EQ(-2.2, joint.getLowerLimit());
+  EXPECT_EQ(1.0, joint.getVelocityLimit());
 
+
+  ros::Duration dt(0.01);
   joint.setPositionCmd(std::numeric_limits<double>::max());
   joint.setVelocityCmd(std::numeric_limits<double>::max());
   joint.setEffortCmd(std::numeric_limits<double>::max());
+  joint.enforceSaturationLimits(dt);
+  double position_upper = joint.getPosition() + dt.toSec() * joint.getVelocityLimit();
+  EXPECT_EQ(position_upper, joint.getPositionCmd());
+  EXPECT_EQ(1.0, joint.getVelocityCmd());
+  EXPECT_EQ(30.0, joint.getEffortCmd());
 
-  EXPECT_EQ(0.0, joint.getPositionCmd());
-  EXPECT_EQ(0.0, joint.getVelocityCmd());
-  EXPECT_EQ(0.0, joint.getEffortCmd());
+  joint.resetCommands(0.0, 0.0, 0.0);
+  joint.setPositionCmd(std::numeric_limits<double>::lowest());
+  joint.setVelocityCmd(std::numeric_limits<double>::lowest());
+  joint.setEffortCmd(std::numeric_limits<double>::lowest());
+  joint.enforceSaturationLimits(dt);
 
-  joint.setPositionCmd(-std::numeric_limits<double>::max());
-  joint.setVelocityCmd(-std::numeric_limits<double>::max());
-  joint.setEffortCmd(-std::numeric_limits<double>::max());
-
-  EXPECT_EQ(0.0, joint.getPositionCmd());
-  EXPECT_EQ(0.0, joint.getVelocityCmd());
-  EXPECT_EQ(0.0, joint.getEffortCmd());
-
+  // The position command is limited in velocity using the previous command and period
+  // to estimate the position reference velocity and clamp it with the velocity effort
+  // In other words, the difference between two successive position commands cannot be
+  // larger than dt*velocity_limit
+  double position_lower = position_upper - dt.toSec() * joint.getVelocityLimit();
+  EXPECT_EQ(position_lower, joint.getPositionCmd());
+  EXPECT_EQ(-1.0, joint.getVelocityCmd());
+  EXPECT_EQ(-30.0, joint.getEffortCmd());
 };
 
-TEST(LimitsTest, softLimitsUrdf){
 
+TEST(LimitsTest, hardLimitsJointGroupUrdf){
+  urdf::Model model;
+//  <limit effort="30" velocity="1.0" lower="-2.2" upper="0.7" />
+  ASSERT_TRUE(model.initParam("/robot_description"));
+
+  gazebo_hw_plugin::JointDataGroup joint_data_group(model);
+  std::vector<std::string> names{"joint1"};
+  ASSERT_TRUE(joint_data_group.initFromNames(names));
+
+  auto joint = joint_data_group.getJoints().front();
+  ASSERT_TRUE(joint->hasLimits());
+  EXPECT_EQ(30.0, joint->getEffortLimit());
+  EXPECT_EQ(0.7, joint->getUpperLimit());
+  EXPECT_EQ(-2.2, joint->getLowerLimit());
+  EXPECT_EQ(1.0, joint->getVelocityLimit());
+
+  ros::Duration dt(0.01);
+  joint->setPositionCmd(std::numeric_limits<double>::max());
+  joint->setVelocityCmd(std::numeric_limits<double>::max());
+  joint->setEffortCmd(std::numeric_limits<double>::max());
+  joint_data_group.enforceLimits(dt);
+  double expected_position_upper = joint->getPosition() + dt.toSec() * joint->getVelocityLimit();
+  EXPECT_EQ(expected_position_upper, joint->getPositionCmd());
+  EXPECT_EQ(1.0, joint->getVelocityCmd());
+  EXPECT_EQ(30.0, joint->getEffortCmd());
+
+
+  joint->resetCommands(0.0, 0.0, 0.0);
+  joint->setPositionCmd(-std::numeric_limits<double>::max());
+  joint->setVelocityCmd(-std::numeric_limits<double>::max());
+  joint->setEffortCmd(-std::numeric_limits<double>::max());
+  joint->enforceSaturationLimits(dt);
+  double expected_position_lower = joint->getPosition() - dt.toSec() * joint->getVelocityLimit();
+  EXPECT_EQ(expected_position_lower, joint->getPositionCmd());
+  EXPECT_EQ(-1.0, joint->getVelocityCmd());
+  EXPECT_EQ(-30.0, joint->getEffortCmd());
 };
+
+//TEST(LimitsTest, softLimitsUrdf){
+//
+//};
 
 
 int main(int argc, char **argv){
